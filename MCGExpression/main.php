ce9a82aa8d2c34ad7f4b3b248af07bc905ed472f
Aditya Saligrama <saligrama427@gmail.com>
2016-09-22 12:38:22 -0400

    Remove test file

--- MCGExpression/main.php
+++ MCGExpression/main.php
@@ -7,16 +7,8 @@
     require("shunting_yard.php");
     require("compare.php");
 
-    $input1 = "-3LOG(4-2PI)-SQRT(2-4PI)";
-    $input2 = "-SQRT(2-3PI)-3LOG(-2PI+4)";
-
     // always run this function first before doing anything else
     // relied upon by tokenizer and shunting_yard
     declare_globals();
-
-    $tok_meta1 = tokenize($input1);
-    $tok_meta2 = tokenize($input2);
-    echo compare(shunting_yard($tok_meta1[0], $tok_meta1[1]),
-                 shunting_yard($tok_meta2[0], $tok_meta2[1])) ? "true" : "false";
 
 ?>
